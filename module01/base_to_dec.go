1c0b6baf38a320d237de13d7bf571946477f240e
Hoshii Natsuki <hoshii.natsuki@hrbrain.co.jp>
2021-05-07 00:01:24 +0900

    Day 2

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -8,6 +8,29 @@ package module01
 //   BaseToDec("E", 16) => 14
 //   BaseToDec("1110", 2) => 14
 //
+// func BaseToDec(value string, base int) int {
+// 	index := "0123456789ABCDEF"
+// 	res := 0.0
+// 	for i, v := range value {
+// 		f := strings.IndexRune(index, v)
+// 		res += float64(f) * math.Pow(float64(base), float64(len(value)-i-1))
+// 	}
+// 	return int(res)
+// }
+
 func BaseToDec(value string, base int) int {
-	return 0
+	index := "0123456789ABCDEF"
+	res := 0
+	multiplyer := 1
+	for i := len(value); i > 0; i-- {
+		v := value[i-1]
+		for j, r := range index {
+			if v == byte(r) {
+				res += j * multiplyer
+				multiplyer *= base
+				break
+			}
+		}
+	}
+	return res
 }
